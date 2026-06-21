e9e9331b2bfaf826556e3a62c8fb9eb5d8d7267a
ribarahonaa <ribarahonaa@gmail.com>
2026-06-21 18:20:57 -0400

    feat(api): solicitudes de upgrade de plan y de anuncio

--- api/src/admin/admin.routes.ts
+++ api/src/admin/admin.routes.ts
@@ -7,6 +7,7 @@ import { requireBranchAccess } from "../middleware/ownership.js";
 import { menuRouter } from "./menu.routes.js";
 import { promotionsRouter } from "./promotions.routes.js";
 import { discountsRouter } from "./discounts.routes.js";
+import { requestsRouter } from "./requests.routes.js";
 
 export const adminRouter = Router();
 
@@ -19,3 +20,4 @@ adminRouter.use("/branches", ownerBranchesRouter);
 adminRouter.use("/branches/:branchId/menu", requireBranchAccess(), menuRouter);
 adminRouter.use("/branches/:branchId/promotions", requireBranchAccess(), promotionsRouter);
 adminRouter.use("/branches/:branchId/discounts", requireBranchAccess(), discountsRouter);
+adminRouter.use("/", requestsRouter);
