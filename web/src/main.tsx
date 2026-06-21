2b640f2f3e23f321637ecd45171bda4a9c941c98
ribarahonaa <ribarahonaa@gmail.com>
2026-06-21 01:42:03 -0400

    feat: add i18n (es/en/pt) and language switcher

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
+import "./i18n/index.js";
 import "./tailwind.css";
 import "leaflet/dist/leaflet.css";
 
